8b5db1ff13b5b7c14d0d04551e749354414ef6ad
Bekzod Akhmadov <exmadovbekzod94@gmail.com>
2025-09-22 13:29:50 +0500

    Add project files.

---
+++ Task 3/Controllers/LcmController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LcmController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string x, [FromQuery] string y)
+        {
+            if (!int.TryParse(x, out int a) || !int.TryParse(y, out int b))
+            {
+                return Content("NaN"); 
+            }
+
+            if (a <= 0 || b <= 0)
+            {
+                return Content("NaN"); 
+            }
+
+            int lcm = a / Gcd(a, b) * b;
+
+            return Content(lcm.ToString());
+        }
+
+        private int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
